57c437cefe3710fe916339823f91020797b834c7
owlalmaty <31688495+owlalmaty@users.noreply.github.com>
2017-11-23 18:16:39 +0600

    Final commit
    the app is finished! all problems solved

---
+++ tetrisIOS/tetris/Swiftris/SquareShape.swift
@@ -0,0 +1,38 @@
+//
+//  Shape.swift
+//  tetris
+//
+//  Created by user on 14.11.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+class SquareShape:Shape {
+    /*
+    
+    | 0•| 1 |
+    | 2 | 3 |
+    
+    • marks the row/column indicator for the shape
+    
+    */
+    
+    // The square shape will not rotate
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.zero:       [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.oneEighty:  [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.ninety:     [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.twoSeventy: [(0, 0), (1, 0), (0, 1), (1, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.oneEighty:  [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.ninety:     [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.twoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+    
+}
